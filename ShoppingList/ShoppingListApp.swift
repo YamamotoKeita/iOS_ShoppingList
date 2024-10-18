92331a3b043f5de09961acef507dad80647b318e
Keita Yamamoto <yamamoto@altonotes.co.jp>
2024-10-18 16:25:20 +0900

    First commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Keita Yamamoto on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
